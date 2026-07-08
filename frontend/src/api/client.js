9eb294d3f0ee374f29685550c953965aec0f91ee
Shikhar Gupta <shikharjul01@gmail.com>
2026-07-08 16:48:20 +0530

    axios timeout for frontend increased

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -7,7 +7,7 @@ export const apiClient = axios.create({
     headers: {
         "Content-Type": "application/json"
     },
-    timeout: 120000,
+    timeout: 500000,
 });
 
 apiClient.interceptors.response.use(
